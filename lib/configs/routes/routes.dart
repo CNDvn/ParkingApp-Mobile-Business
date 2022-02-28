5539076f442496fcf9264fbf4d222ebfff04c4d9
Code Never Die <banhduchieucode@gmail.com>
2022-02-28 21:29:52 +0700

    done

--- lib/configs/routes/routes.dart
+++ lib/configs/routes/routes.dart
@@ -5,9 +5,8 @@ import 'package:parking_app_mobile_business/views/root.dart';
 
 class Routes {
   static final Map<String, Widget Function(BuildContext)> routes = {
-    // "/": (context) => const Root(),
-    // "/parkingManagement": (context) => const ParkingManagementPage(),
-    // "/createParking": (context) => const NewParkingPage(),
-    "/": (context) => const ParkingManagementPage(),
+    "/": (context) => const Root(),
+    "/parkingManagement": (context) => const ParkingManagementPage(),
+    "/createParking": (context) => const NewParkingPage(),
   };
 }
