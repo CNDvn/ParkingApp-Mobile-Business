d427a105e8d2b22704d0c091812e3a218d329569
DQLok <wkhhkw01@gmail.com>
2022-03-17 21:46:21 +0700

    change code

--- lib/views/priceList/price_list_page.dart
+++ lib/views/priceList/price_list_page.dart
@@ -27,7 +27,6 @@ class PriceListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
     return Container(
       decoration: BoxDecoration(
           boxShadow: const [
